5499e7dd1ab82d54ea22cd876d7682c477e52ebc
chenyi5 <chenyi5@joyy.com>
2025-03-26 09:55:25 +0800

    update: 新增TokenParser

--- tool/token_extractor.go
+++ tool/token_extractor.go
@@ -1,5 +1,10 @@
 package tool
 
+import (
+	"strings"
+	"unicode"
+)
+
 type TokenExtractor struct {
 	start []rune
 	end   []rune
@@ -74,4 +79,61 @@ func (extractor *TokenExtractor) find(target []rune, chars []rune, cursor int) i
 		}
 	}
 	return -1
+}
+
+// TokenParser 匹配start和end，并执行替换函数；中间的key执行apply进行替换
+// start不可为空,end可以为空;e.g.: @test
+func TokenParser(text, start, end string, apply func(key string) string) string {
+	if text == "" || start == "" {
+		return text
+	}
+	builder := strings.Builder{}
+	startChars := []rune(start)
+	endChars := []rune(end)
+	textChars := []rune(text)
+	startIdx := -1
+	tail := 0
+	for cursor := 0; cursor < len(textChars); {
+		char := textChars[cursor]
+		isSpace := IsSpace(char)
+
+		if startIdx != -1 && ((isSpace && len(endChars) == 0) ||
+			(len(endChars) > 0 && matchChars(textChars, endChars, cursor))) {
+			builder.WriteString(string(textChars[tail:startIdx]))
+			key := string(textChars[startIdx+len(startChars) : cursor])
+			replace := apply(key)
+			builder.WriteString(replace)
+			tail = cursor + len(endChars)
+			if cursor == tail {
+				cursor++
+			} else {
+				cursor = tail
+			}
+			startIdx = -1
+			continue
+		} else if isSpace {
+			startIdx = -1
+		} else if matchChars(textChars, startChars, cursor) {
+			startIdx = cursor
+		}
+		cursor++
+	}
+	if tail < len(textChars) {
+		builder.WriteString(string(textChars[tail:]))
+	}
+	return builder.String()
+}
+
+func IsSpace(char rune) bool {
+	return unicode.IsSpace(char)
+}
+
+func matchChars(values, target []rune, idx int) bool {
+	for i := 0; i < len(target); i++ {
+		cursor := idx + i
+		if cursor >= len(values) || values[cursor] != target[i] {
+			return false
+		}
+	}
+	return true
 }
---
+++ tool/tool_test.go
@@ -0,0 +1,14 @@
+package tool
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTokenParser(t *testing.T) {
+	sql := "select * from a where a = #{test} and b = #{test2} and c = 1"
+	parser := TokenParser(sql, "#{", "}", func(key string) string {
+		return "@" + key
+	})
+	fmt.Println(parser)
+}
